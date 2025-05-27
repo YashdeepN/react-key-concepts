cd0343f74d6739986485b6502820f9cc2aaac1d8
YashdeepN <yashdeepneema@hotmail.com>
2025-05-27 11:13:40 +0530

    Preferences Checkbox component

--- src/components/RefsInCustomCompo/Preferences.jsx
+++ src/components/RefsInCustomCompo/Preferences.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Preferences = () => {
-    return (
-        <div>Preferences</div>
-    )
-}
+  const [wantsProdInfo, setWantsProdInfo] = useState(false);
+  const [wantsProdUpdate, setWantsProdUpdate] = useState(false);
+  return (
+    <div className={classes.preferences}>
+      <label>
+        <input
+          type="checkbox"
+          id="pref-new"
+          checked={wantsProdInfo}
+          onChange={handleChangeProdInfo}
+        />
+        <span>New Product</span>
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          id="pref-update"
+          checked={wantsProdUpdate}
+          onChange={handleChangeProdUpdate}
+        />
+        <span>Product Update</span>
+      </label>
+    </div>
+  );
+};
 
-export default Preferences
+export default Preferences;
