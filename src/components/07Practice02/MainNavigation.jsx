767071bfcb23aa26adc2021d72858cd95e4af137
YashdeepN <yashdeepneema@hotmail.com>
2025-05-29 11:19:37 +0530

    portal side bar-- main navigation first commit

---
+++ src/components/07Practice02/MainNavigation.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./MainNavigation.module.css";
+
+const MainNavigation = () => {
+  return <div>MainNavigation</div>;
+};
+
+export default MainNavigation;
