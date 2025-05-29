83da719fb77dff4a04ae3bcb4ed93aac85259660
YashdeepN <yashdeepneema@hotmail.com>
2025-05-29 10:10:16 +0530

    ref in input and select and log their values

--- src/components/07Practice/Form.jsx
+++ src/components/07Practice/Form.jsx
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
 import classes from "./Form.module.css";
 import "./Form.module.css";
 
 const Form = () => {
+  const nameRef = useRef();
+  const ProgramRef = useRef();
+
   function handleSubmit(event) {
     event.preventDefault();
+    console.log(nameRef.current.value);
+    console.log(ProgramRef.current.value);
   }
   return (
     <form onSubmit={handleSubmit}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input id="name" type="text" />
+        <input id="name" type="text" ref={nameRef} />
       </div>
       <div className="form-control">
-        <label htmlFor="program"></label>
-        <select name="program" id="program">
+        <label htmlFor="program">Choose your program </label>
+        <select name="program" id="program" ref={ProgramRef}>
           <option value="basic">Basic</option>
           <option value="advanced">Advanced</option>
           <option value="mastery">Mastery</option>
