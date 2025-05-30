a374d93e35d7fe453713803a9eeb2b9946f5d48a
YashdeepN <yashdeepneema@hotmail.com>
2025-05-30 11:56:03 +0530

    Form Action component markup

--- src/components/FormAction.jsx
+++ src/components/FormAction.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const FormAction = () => {
-  return <div>FormAction</div>;
+  return (
+    <form action={submitAction}>
+      <p>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+      </p>
+      <p>
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" />
+      </p>
+      <p>
+        <button>Login</button>
+      </p>
+    </form>
+  );
 };
 
 export default FormAction;
